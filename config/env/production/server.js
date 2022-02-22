99223f720fb40b928fc9acb6e6fcc64ce57c126c
Felipe Amaya <felipea.0420@gmail.com>
2022-02-21 19:44:36 -0500

    production server keys seted x2

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
     host: env('HOST', 'pipecode-rest-api.herokuapp.com'),
     port: env.int('PORT', 1337),
     app: {
-        keys: env.array('APP_KEYS'),
+        keys: env.array("APP_KEYS", ["1dc7ffcca6136ed542b89318e5f22501", "d598da162aac4be59534f231ad287acc"]),
     },
 });
