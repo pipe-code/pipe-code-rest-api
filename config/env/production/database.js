3328e29b971edc8290e3d4f934b84acac4601795
Felipe Amaya <felipea.0420@gmail.com>
2022-02-21 19:28:14 -0500

    mysql production credentials updated

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'mysql',
         connection: {
-            host: env('DATABASE_HOST', 'us-cdbr-east.cleardb.com'),
+            host: env('DATABASE_HOST', 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             port: env.int('DATABASE_PORT', 3306),
-            database: env('DATABASE_NAME', 'heroku_4e92ecd80c2b874'),
-            user: env('DATABASE_USERNAME', 'bfaeb3179d58d8'),
-            password: env('DATABASE_PASSWORD', '48584ed0'),
+            database: env('DATABASE_NAME', 'fzdssd220uhay00u'),
+            user: env('DATABASE_USERNAME', 'e8sks4qher6u1or6'),
+            password: env('DATABASE_PASSWORD', 'mm6nnvc7f3qmw3ap'),
             ssl: {
                 rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
             },
